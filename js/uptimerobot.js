884bf541223625031784a8c5f67c1d720d427004
pukaiting <1096941674@qq.com>
2021-06-08 15:44:34 +0800

    Site updated: 2021-06-08 15:44:33

--- js/uptimerobot.js
+++ js/uptimerobot.js
@@ -2,7 +2,7 @@ var __apiKeys = [
 	'm788340453-b2f73bb5b9993dbb2a1a36f1',// 繁星小站
 	'm788340629-04af52c5b717f904d9ce8dc3',// 南城破事啊
 	'm788340623-6fef5cef6d1d0c67d213124d',// 快收录
-	
+	'm788353284-14a36d3a7ef3e7cda832be2a',//小站
 
 ];
 //https://uptimerobot.com/ 设置要监控的域名或者ip 获取到key
