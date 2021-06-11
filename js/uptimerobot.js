cac301c1c3647360282c0854c6240b452a6c3fd0
pukaiting <1096941674@qq.com>
2021-06-11 14:37:48 +0800

    Site updated: 2021-06-11 14:37:47

--- js/uptimerobot.js
+++ js/uptimerobot.js
@@ -2,8 +2,12 @@ var __apiKeys = [
 	'm788340453-b2f73bb5b9993dbb2a1a36f1',// 繁星小站
 	'm788340629-04af52c5b717f904d9ce8dc3',// 南城破事啊
 	'm788340623-6fef5cef6d1d0c67d213124d',// 快收录
-	'm788353284-14a36d3a7ef3e7cda832be2a',//小站
-
+	'm788383297-bed3fe9ac7cd05efea535efc',//小冰博客
+    'm788383299-4dcfa95fedca9a27fd78058e',//山河
+    'm788383337-f99bd94b531ed4b504ab10ad',//学习与生活
+    'm788383348-a19ad319223c9e64adb72141',//零下37度心温
+    'm788383360-ff172b1998ce41eea66d465c',//一木博客
+    'm788383363-f60441dee3bf8dc34317b727',//TAOG的小站
 ];
 //https://uptimerobot.com/ 设置要监控的域名或者ip 获取到key
 
